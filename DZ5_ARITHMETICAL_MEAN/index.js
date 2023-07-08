05bae4efe7dcbb0aab4b8efd6f860f19bde3c5b9
WND <54204831+m0o0n@users.noreply.github.com>
2023-07-08 16:52:00 +0300

    Update index.js
    fix condition to isValid

--- DZ5_ARITHMETICAL_MEAN/index.js
+++ DZ5_ARITHMETICAL_MEAN/index.js
@@ -5,11 +5,11 @@ average.addEventListener('click', ()=>{
     const second = prompt('Enter second value')
     const third = prompt('Enter third value')
     const sum = Number(first) + Number(second) + Number(third)
-    const condition = ((first && first.match(/^-\d{1,}|\d{1,}/)) 
+    const isValid = ((first && first.match(/^-\d{1,}|\d{1,}/)) 
     && (second && second.match(/^-\d{1,}|\d{1,}/)) 
     && (third && third.match(/^-\d{1,}|\d{1,}/)))
 
-    if(condition){ 
+    if(isValid){ 
         alert(`Arithmetical mean equals: ${(sum/3).toFixed(2)}`)
     } else {
         alert('one of prompts is incorect value')
