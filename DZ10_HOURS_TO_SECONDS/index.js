70528fffaed03801e757297d01277684fc887dfe
m0o0n <strikunvlad@gmail.com>
2023-07-15 13:35:31 +0300

    feat: dz10

---
+++ DZ10_HOURS_TO_SECONDS/index.js
@@ -0,0 +1,12 @@
+const calculateBtn = document.querySelector('#calculate')
+
+calculateBtn.addEventListener('click', () => {
+    const hours = prompt('Please enter quantity of hours to convert in seconds (it should be the number)')
+    if(hours && !isNaN(hours)){
+        const hoursToPositiveNum = Math.abs(Number(hours))
+        const seconds = hoursToPositiveNum * 3600
+        alert(`${hoursToPositiveNum} hours equals ${seconds} seconds`)
+    } else {
+        alert('You enter incorect value')
+    }
+})
