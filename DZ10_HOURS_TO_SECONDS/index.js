1204f285bb1cd455574ea14d9d6a2baae6dc66d0
WND <54204831+m0o0n@users.noreply.github.com>
2023-07-15 14:59:43 +0300

    Update index.js

--- DZ10_HOURS_TO_SECONDS/index.js
+++ DZ10_HOURS_TO_SECONDS/index.js
@@ -3,7 +3,7 @@ const calculateBtn = document.querySelector('#calculate')
 calculateBtn.addEventListener('click', () => {
     const hours = prompt('Please enter quantity of hours to convert in seconds (it should be the number)')
     if(hours && !isNaN(hours)){
-        const hoursToPositiveNum = Math.abs(Number(hours))
+        const hoursToPositiveNum = Math.abs(hours)
         const seconds = hoursToPositiveNum * 3600
         alert(`${hoursToPositiveNum} hours equals ${seconds} seconds`)
     } else {
