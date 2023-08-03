409fca33b9968fa870e1b23c6d4db9bcfb61ac0d
m0o0n <strikunvlad@gmail.com>
2023-08-03 17:53:22 +0300

    fix: dz24

--- DZ24_STUDENT/index.js
+++ DZ24_STUDENT/index.js
@@ -7,41 +7,30 @@ class Student {
     grades = []
     attendance = new Array(25)
     iterator = 0
-    makeGrage(mark){
-        this.grades.push(mark)
-        return this
-    }
-    summary(){
-        console.log(this.grades, this.attendance)
-        const countOfLessons = this.attendance.filter(l => l !== undefined).length
-        const countOfPresent = this.attendance.filter(l => l === true).length
-        const meanMark = countOfLessons ? this.grades.reduce((acc, cur) => acc += cur, 0) / countOfLessons : 1
-        const meanPresent = countOfLessons ? countOfPresent / countOfLessons : 1
-        switch(true){
-            case meanPresent >= 0.9 && meanMark >= 90:
-                return "Молодець!"
-            case meanPresent < 0.9 && meanMark >= 90 || meanPresent >= 0.9 && meanMark < 90:
-                return "Добре, але можна краще "
-            default: return "Редиска!"
-        }
-        
-    }
+    
     getAge(){
         const curentDate = new Date
         return `Возраст ${curentDate.getFullYear() - this.year}`
     }
     getMeanGrade(){
         const countOfLessons = this.attendance.filter(l => l !== undefined).length
-        const meanMark = countOfLessons ? this.grades.reduce((acc, cur) => acc += cur, 0) / countOfLessons : 1
+        const meanMark = countOfLessons ? this.grades.reduce((acc, cur) => acc += cur, 0) / countOfLessons : "Оценка отсутсвует"
         return `Средняя оценка ${meanMark}`
+    }
+    _makeGrage(mark){
+        this.grades.push(mark)
+    }
+    fixGrade(lessonIndex, mark) {
+        this.grades[lessonIndex] = mark
+        return this
     }
     present(mark = 0){
         if(this.iterator < 25){
             this.attendance[this.iterator] = true
             this.iterator++
-            this.makeGrage(mark)
+            this._makeGrage(mark)
         } else {
-            console.log('Больше нет мест в ячейках для посешния')
+            console.log('Больше нет мест в ячейках для посещения')
         }
         return this
     }
@@ -49,12 +38,43 @@ class Student {
         if(this.iterator < 25){
             this.attendance[this.iterator] = false
             this.iterator++
+            this._makeGrage(0)
         } else {
-            console.log('Больше нет мест в ячейках для посешния')
+            console.log('Больше нет мест в ячейках для посещения')
         }
         return this
     }
-    
+
+    summary(){
+        console.log(this.grades, this.attendance)
+        const countOfLessons = this.attendance.filter(l => l !== undefined).length
+        const countOfPresent = this.attendance.filter(l => l === true).length
+
+        //Вариант 1
+        // тут сделал среднюю оценку в зависимости от кол-ва уроков по принципу что за каждый урок ученик получает оценку 
+        const meanMark = countOfLessons ? this.grades.reduce((acc, cur) => acc += cur, 0) / countOfLessons : null
+
+        //Вариант 2
+        // тут сделал среднюю оценку в зависимости от кол-ва оценок по принципу что оценок можно получить любое количество
+        // но тогда метод _makeGrage уже не будет приватным
+        // const meanMark = this.grades.length ? this.grades.reduce((acc, cur) => acc += cur, 0) / this.grades.length : 0
+
+
+        //В условии не написано как правильно должна считаться средняя оценка так что оставлю вариант который мне нравиться 
+
+
+        const meanPresent = countOfLessons ? countOfPresent / countOfLessons : null
+        switch(true){
+            case meanPresent >= 0.9 && meanMark >= 90:
+                return "Молодець!"
+            case meanPresent < 0.9 && meanMark >= 90 || meanPresent >= 0.9 && meanMark < 90:
+                return "Добре, але можна краще"
+            case countOfLessons === 0:
+                return "Поки зарано щось казати"
+            default: return "Редиска!"
+        }
+        
+    }
 }
 
 const vlad = new Student('Vlad', 'Shtrikun', 2000)
@@ -86,6 +106,7 @@ vlad
 .present(100)
 .present(100)
 
+// vlad.fixGrade(1, 100).fixGrade(2, 100).fixGrade(3, 100)
 console.log(vlad.summary())
 console.log(vlad.getAge())
 console.log(vlad.getMeanGrade())
