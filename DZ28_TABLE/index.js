57797f05483c65df31915d53331bb1c000236156
m0o0n <strikunvlad@gmail.com>
2023-08-13 15:27:18 +0300

    feat: dz28

---
+++ DZ28_TABLE/index.js
@@ -0,0 +1,12 @@
+const table = document.querySelector('table')
+
+let arr = [] 
+for(let i = 1; i <= 10; i++){
+    const tr = document.createElement('tr')
+    for (let j = i*10 - 9; j <= i*10; j++) {
+        const td = document.createElement('td')
+        td.innerText = j
+        tr.append(td)
+    }
+    table.append(tr)
+}
