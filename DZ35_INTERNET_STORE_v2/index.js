9c79f62f5603134e424432902354eb40c7ac025e
m0o0n <strikunvlad@gmail.com>
2023-08-20 14:23:29 +0300

    feat: dz35

--- DZ35_INTERNET_STORE_v2/index.js
+++ DZ35_INTERNET_STORE_v2/index.js
@@ -16,9 +16,17 @@ const categories = [
     { id: 2, title: "Холодильники" },
 ]
 
+let currentProduct = null
+
+
 const nav = document.querySelector('.nav')
 const productList = document.querySelector('.products')
 const info = document.querySelector('.info')
+const modal = document.querySelector('.modal')
+const modalClose = document.querySelector('.modal #close')
+const modalForm = document.querySelector('.modal form')
+const modalInfoContainer = document.querySelector('.modal .info_container')
+const modalContentContainer = document.querySelector('.modal .content_container')
 
 categories.forEach((e) => {
     const span = document.createElement('span')
@@ -46,13 +54,13 @@ nav.addEventListener('click', (e) => {
         e.target.classList.add('active')
         info.innerHTML = ''
     }
-    
+
 })
 
-productList.addEventListener('click', (e)=>{
-    if(e.target.tagName === "H2" || e.target.tagName === "BUTTON"){
+productList.addEventListener('click', (e) => {
+    if (e.target.tagName === "H2" || e.target.tagName === "BUTTON") {
         const currentProductId = Number(e.target.parentNode.dataset.id)
-        const currentProduct = products.find(e => e.id === currentProductId)
+        currentProduct = products.find(e => e.id === currentProductId)
         info.innerHTML = `
             <h1>${currentProduct.title}</h1>
                 <p>
@@ -61,16 +69,82 @@ productList.addEventListener('click', (e)=>{
                 <p>
                     ${currentProduct.info}
                 </p>
-            <button>Купить</button>
+            <button data-id="${currentProductId}">Купить</button>
         `
     }
 })
 
-info.addEventListener('click', (e)=>{
-    if(e.target.tagName === "BUTTON"){
-        alert('Товар придбано')
-        productList.innerHTML = ''
-        info.innerHTML = ''
-        nav.querySelector('span.active').classList.remove('active')
+info.addEventListener('click', (e) => {
+    if (e.target.tagName === "BUTTON") {
+        const { title, year, info } = currentProduct
+        modal.style.display = "flex"
+        modalContentContainer.style.display = "flex"
+        document.querySelector('.modal .product_card').innerHTML = `
+        <h2>${title}</h2>
+            <p><strong>Year: </strong>${year}</p>
+        <p>${info}</p>
+    `
+        
     }
+})
+
+function isValidForm({name, city, count, shiping}) {
+    const isName = name.trim() && name.length < 30 && name.length > 5 ? false : 'Тут есть ошибка'
+    const isCity = city ? false : 'Это поле обязательно'
+    const isCount = Number(count) > 0 && Number(count) <= 5 ? false : 'Тут есть ошибка'
+    const isShiping = shiping.trim() && shiping.length < 40 ? false : 'Тут есть ошибка'
+    if (!isCity && !isCount && !isShiping && !isName) {
+        return { status: true }
+    } else {
+        return { status: false, isCity, isCount, isShiping, isName }
+    }
+}
+
+modalForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const fd = [...(new FormData(e.currentTarget))]
+    const data = Object.fromEntries(fd)
+    const {name, city, shiping, pay, count, comment} = data
+    if (isValidForm(data).status) {
+        document.querySelectorAll('.modal form span.err').forEach((e) => {
+            e.innerText = ''
+        })
+
+        modalForm.reset()
+
+        modalContentContainer.style.display = 'none'
+        modalInfoContainer.style.display = "flex"
+
+        modalInfoContainer.innerHTML = `
+            <span><strong>Your name: </strong>${name}</span>
+            <span><strong>Your city: </strong>${city}</span>
+            <span><strong>Your shiping address: </strong>${shiping}</span>
+            <span><strong>Your pay method: </strong>${pay}</span>
+            <span><strong>Your going to by ${count}: </strong>${currentProduct.title}</span>
+            <span><strong>Comment: </strong>${comment}</span>
+        `
+    } else {
+        const { isName, isCity, isCount, isShiping } = isValidForm(data)
+        if (isName) {
+            document.querySelector('.modal form *[name="name"]').nextElementSibling.innerText = isName
+        }
+
+        if (isCity) {
+            document.querySelector('.modal form *[name="city"]').nextElementSibling.innerText = isCity
+        }
+
+        if (isCount) {
+            document.querySelector('.modal form *[name="count"]').nextElementSibling.innerText = isCount
+        }
+
+        if (isShiping) {
+            document.querySelector('.modal form *[name="shiping"]').nextElementSibling.innerText = isShiping
+        }
+    }
+})
+
+
+modalClose.addEventListener('click', () => {
+    modalInfoContainer.style.display = "none"
+    modal.style.display = "none"
 })
